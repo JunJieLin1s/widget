35979f308f48173ede0fafde40dcdff777ea621e
Jun Jie Lin <junjie@vitrion.nl>
2025-02-26 13:11:56 +0100

    Revert "."
    This reverts commit 7cd64925528da803d4c33422da75bf81cfdff956.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import Navbar from './components/Navbar.vue'
 import Dashboard from './components/Dashboard.vue'
 </script>
 
 <template>
-  <Navbar />
   <Dashboard />
 </template>
